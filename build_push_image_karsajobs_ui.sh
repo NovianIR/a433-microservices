32fcff454d182a4f301c9e847e664b240f488884
Novian Ilham Ramadhan <novianilham321@gmail.com>
2024-03-26 15:49:42 +0000

    Pesan commit Anda di sini

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -4,7 +4,7 @@
 docker build -t novianilham/karsajobs-ui:latest .
 
 # Login to Docker Hub
-docker login -u novianilham -p NggakBolehTahu8
+echo $PASSWORD_DOCKER_HUB | docker login -u novianilham --password-stdin
 
 # Push image to Docker Hub
 docker push novianilham/karsajobs-ui:latest
